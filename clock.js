ae698ea3bdf1918f662889774c8be5f97a95ee2d
Ansel Colby <dcolby1@macalester.edu>
2019-03-05 11:41:45 -0600

    Minor changes

--- clock.js
+++ clock.js
@@ -26,6 +26,9 @@ const sheetName = ''; // TODO Put your sheet name here -> the name of the sheet 
 
 main();
 
+/*
+ * The main async handler
+ */
 async function main() {
   console.log('>>>> authenticating . . .');
   try {
@@ -39,23 +42,22 @@ async function main() {
       rows = await updateRows(rows, input);
       await clockOut(auth, rows);
     }
-    console.log('d o n e');
+    console.log('done');
   } catch (e) {
-    console.log('uh oh there was an error ', e);
-    throw new Error('SHIT');
+    console.log(e);
   }
 }
 
 /**
  * Gets input from the user from the console
- * Returns an object of clock (in or out) and time
+ * @returns an object containing the clock method and the time
  */
 async function getUserInput() {
   console.log('>>>> valid authentication');
   let inOrOut = readline.question('>>> Would you like to clock in or out? (i/o) ');
   if (inOrOut !== 'i' && inOrOut !== 'o') {
     console.log(inOrOut);
-    throw new Error('invalid input!!!!!');
+    throw new Error('invalid input');
   }
   let timeclock = readline.question('>>> What time are you clocking in? ');
   let input = {
@@ -67,6 +69,8 @@ async function getUserInput() {
 
 /**
  * Appends a row at the bottom of the spreadsheet
+ * @param auth the Google auth
+ * @param input the input object returned from getUserInput()
  */
 async function clockIn(auth, input) {
   console.log('>>> clocking in . . .');
@@ -92,26 +96,29 @@ async function clockIn(auth, input) {
   try {
     let res = await append(request);
   } catch (e) {
-    throw new Error('The API returned an error: ', e);
+    throw(e);
   }
   console.log('>>> clocked in!');
 }
 
 /**
- *
+ * Retrieves the rows from the sheet in order to modify and append
+ * @param auth the Google auth
+ * @returns the rows retrieved
  */
 async function getRows(auth) {
   console.log('>>>> retrieving row data . . .');
   const params = {
     auth: auth,
     spreadsheetId: spreadsheetId,
-    ranges: `${sheetName}!A5:F`,
+    ranges: `${sheetName}!A:F`,
     includeGridData: true
   }
 
   try {
     const response = await getSheet(params);
     let rows = response.sheets[0].data[0].rowData;
+    console.log(rows)
 
     if (rows.length  > 0) {
       console.log('>>>> retrieved row data');
@@ -121,50 +128,57 @@ async function getRows(auth) {
     }
 
   } catch (e) {
-    console.error('>>>> The API returned an error: ');
-    console.log(e);
-    return null;
+    throw(e);
   }
 }
 
 /**
- *
+ * Modifies the rows to be appended later
+ * @param rows the rows object returned from getRows()
+ * @param input the input object returned from getUserInput()
+ * @returns the updated rows object
  */
 async function updateRows(rows, input) {
-  let index = false;
-  for (let i = 0; i < rows.length; i++) {
-    let row = rows[i].values;
-    if (row) {
-      for (var j = 0; j < row.length; j++) {
-        let cell = row[j];
-        if (cell.formattedValue && (cell.formattedValue === todaysDate)) {
-          index = true;
-          row[j+2].userEnteredValue = {stringValue: input.timeclock};
-          row[j+2].userEnteredFormat.horizontalAlignment = 'RIGHT';
+  try {
+    let index = false;
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i].values;
+      if (row) {
+        for (var j = 0; j < row.length; j++) {
+          let cell = row[j];
+          if (cell.formattedValue && (cell.formattedValue === todaysDate)) {
+            index = true;
+            row[j+2].userEnteredValue = {stringValue: input.timeclock};
+            row[j+2].userEnteredFormat.horizontalAlignment = 'RIGHT';
 
-          let start = moment(row[j+1].formattedValue, 'h:mm A');
-          let end = moment(input.timeclock, 'h:mm A');
-          let diff = Math.abs(start.diff(end, 'hours', 'minutes'));
-          row[j+3].userEnteredValue = {numberValue: diff};
+            let start = moment(row[j+1].formattedValue, 'h:mm A');
+            let end = moment(input.timeclock, 'h:mm A');
+            let diff = Math.abs(start.diff(end, 'hours', 'minutes'));
+            row[j+3].userEnteredValue = {numberValue: diff};
 
-          // console.log(rows[i-1]);
+            // console.log(rows[i-1]);
 
-          if (rows[i-1].values[j+4].effectiveFormat.textFormat.bold) {
-            row[j+4].userEnteredValue = {numberValue: parseFloat(diff.toString()).toFixed(2)};
-          } else {
-            row[j+4].userEnteredValue = {numberValue: Number(rows[i-1].values[j+4].formattedValue) + diff};
+            if (rows[i-1].values[j+4].effectiveFormat.textFormat.bold) {
+              row[j+4].userEnteredValue = {numberValue: parseFloat(diff.toString()).toFixed(2)};
+            } else {
+              row[j+4].userEnteredValue = {numberValue: Number(rows[i-1].values[j+4].formattedValue) + diff};
+            }
           }
         }
       }
     }
-  }
-  if (!index) throw new Error("NO CLOCK IN FOUND FOR TODAY'S DATE");
+    if (!index) throw new Error("NO CLOCK IN FOUND FOR TODAY'S DATE");
 
-  return rows;
+    return rows;
+  } catch (e) {
+    throw(e);
+  }
 }
 
 /**
- *
+ * Takes in the modified rows and appends them to the sheet
+ * @param auth the Google auth
+ * @param rows the modified rows from updateRows()
  */
 async function clockOut(auth, rows) {
   console.log('>>>> updating rows . . .');
@@ -190,6 +204,6 @@ async function clockOut(auth, rows) {
     let res = await batchUpdate(request);
     console.log('>>>> updated the sheet');
   } catch (e) {
-    console.log('>>>> The API returned an error: ', e);
+    throw(e);
   }
 }
